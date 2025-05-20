7a8dfd7aad328a1d683dfc3211549094f70585bc
Martin Odongo <martinwere14@gmail.com>
2025-05-20 19:40:24 +0300

    first alx commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+operation = input("Enter an operation(+, -,*, /)  to perform: ")
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result =number1 - number2
+elif operation == "*":
+    result = number1* number2
+elif operation == number1/number2:
+    result=number1/number2
+else:
+
+    print("Invalid operation. Please enter one of the following: +, -, *, /")
+    exit()
+print(f"The result of {number1} {operation} {number2} is: {result}")
+
