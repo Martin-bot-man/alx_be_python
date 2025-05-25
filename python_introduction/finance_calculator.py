b4887501fdae42f1616d9b79984bbaf548afa93f
Martin Odongo <martinwere14@gmail.com>
2025-05-25 09:14:01 +0300

    python monthly expense calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,6 +5,6 @@ monthly_savings = monthly_income - monthly_expense;
 
 annual_interest_rate = 0.05;
 
-Projected_savings = monthly_savings * 12 + (monthly_savings*12*0.05);
+Projected_savings = monthly_savings * 12 + float(monthly_savings*12*0.05);
 
 print(f"Your projected savings for the year is: {Projected_savings:.2f}");
