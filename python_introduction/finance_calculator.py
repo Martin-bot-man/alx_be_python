c574726b2c6d3632b641845c869636e0f321ae01
Martin Odongo <martinwere14@gmail.com>
2025-05-25 09:16:56 +0300

    expense calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income= int(input("Enter your monthly income:"));
 monthly_expense = int(input ("Enter your total monthly expenses:"));
 
-monthly_savings = monthly_income - monthly_expense;
+monthly_savings = float(monthly_income) - float(monthly_expense);
 
 annual_interest_rate = 0.05;
 
