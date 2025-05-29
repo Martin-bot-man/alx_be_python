ba0435d9c2bd92a2fe37a67565a509ff52974d63
Martin Odongo <martinwere14@gmail.com>
2025-05-29 05:28:11 +0300

    nested loops

--- multiplication.py
+++ multiplication.py
@@ -1,3 +1,7 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i} *{j} = {i*j}" , end ="/t")
+rows = 5
+
+for i in range(1, rows + 11):
+    for j in range (1, i+ 1):
+
+        print("*")
+        
