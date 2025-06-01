be5f24322d06e54f51f1db2aff3b26e278ad0772
Martin Odongo <martinwere14@gmail.com>
2025-06-01 09:42:59 +0300

    calculator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ num1 = int (input ("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     result = num1* i
-    print(f"{num1} * {i} = {result}")
+print(f"{num1} * {i} = {result}")
