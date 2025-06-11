eb386108ae16d4017415b4b2b044e76632d246fa
Martin Odongo <martinwere14@gmail.com>
2025-06-11 05:59:05 +0300

    alx calculator

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -4,10 +4,10 @@ def main():
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Choose the operation (add, subtract, multiply, divide: " ).stip().lower()
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
 
 if __name__ == "__main__":
-    main()   
+    main()
