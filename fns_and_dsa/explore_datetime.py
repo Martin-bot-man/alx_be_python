3709de5239ad28eadef0e728cf8d724a6a47869f
Martin Odongo <martinwere14@gmail.com>
2025-06-13 01:59:20 +0300

    feat: used timedelta to add future dates.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,7 +6,7 @@ def display_current_datetime():
     print(f"Current date and time :{current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
 def calculate_future_date():
-    days_to_add = int(input("Enter the number of days to add: "))
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
     future_date  = datetime.now() + timedelta(days = days_to_add)
     print(f'Future date after adding{days_to_add} days: {future_date.strftime("%Y-%m-%d %H:%M:%S")}')
 if __name__ == "__main__":
