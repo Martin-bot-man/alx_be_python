eaf656f247c288b5478467bcf6db22e6ae5821c9
Martin Odongo <martinwere14@gmail.com>
2025-06-14 05:36:14 +0300

    feat : alx temperature calculator

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,24 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR= 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+user_input = float(input('Enter the temperature: '))
+prompt = input('Is this in Fahrenheit or Celsius? (F/C): ').strip().upper()
+def convert_to_celsius(fahrenheit):
+    
+        return(fahrenheit -32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+
+def convert_to_fahrenheit(celsius):
+    
+    return(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+if prompt  == "F":
+      converted_temp = convert_to_celsius(user_input)
+      print(f"{user_input}°F is equal to {converted_temp:.2f}°C")
+elif prompt == "C":
+      converted_temp = convert_to_fahrenheit(user_input)
+      print(f"{user_input}°C is equal to {converted_temp:.2f}°F")
+else: 
+      print("Invalid input. Please enter 'F' for Fahrenheit or 'C' for Celsius.")     
+
