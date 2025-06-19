964f539f4f252221c24074efef394ce6e2de1f90
Martin Odongo <martinwere14@gmail.com>
2025-06-19 03:15:04 +0300

    feat: created the main file

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,30 +2,27 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)#Example starting balance
-    if len(sys.argv) > 2:
-        print("Usage: python main-o.py <command>: <amount>")
-        print("commands: deposit, withdraw, display")
+    account = BankAccount(100)  # Example starting balance
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <command>:<amount>")
+        print("Commands: deposit, withdraw, display")
         sys.exit(1)
 
     command, *params = sys.argv[1].split(':')
-    amount = float(params[0]) if params else 0
+    amount = float(params[0]) if params else None
 
-    if command == "deposit"and amount is not None:
-         account.deposit(amount)
-         print(f"Deposited{amount}.New balance: {account .display_balance()}")
-    elif command =="withdraw" and amount is not None:
+    if command == "deposit" and amount is not None:
+        account.deposit(amount)
+        print(f"Deposited: ${amount}")
+    elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew{amount}")     
+            print(f"Withdrew: ${amount}")
         else:
-            print("insufficient funds")
-    elif command =="display":
-        print(f"Current balance: {account.display_balance()}")  
-
+            print("Insufficient funds.")
+    elif command == "display":
+        account.display_balance()
     else:
-        print("Invalid command")  
-if __name__ =="__main__":
-    main()           
+        print("Invalid command.")
 
-
-
+if __name__ == "__main__":
+    main()
