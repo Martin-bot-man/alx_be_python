5954adec0ad5cb7588b1cfbf47b05531ded63365
Martin Odongo <martinwere14@gmail.com>
2025-06-20 04:17:59 +0300

    feat: added error handling parametors

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,14 +1,20 @@
 
 def safe_divide(numerator, denominator):
     try:
-        if denominator == 0:
-            raise ZeroDivisionError("Denominator cannot be zero.")
+        num = float(numerator)
+        den = float(denominator)
         
-        result = numerator / denominator
+        result = num / den
         print (f"result: {result}")
+
     except ZeroDivisionError as e:
         return str(e)
+    
     except TypeError as e:
         return "Invalid input: " + str(e)
+    
+    except ValueError as e:
+        return "Invalid input: " + str(e)
+safe_divide()    
 
     
