1c68055e7a57d329119aed28d8efd9de55b412e8
Martin Odongo <martinwere14@gmail.com>
2025-06-21 02:04:54 +0300

    feat: robust division error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,20 +1,18 @@
 
+
 def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
         den = float(denominator)
         
         result = num / den
-        print (f"result: {result}")
+        return f"Result: {result}"
 
     except ZeroDivisionError as e:
         return str(e)
-    
-    except TypeError as e:
-        return "Invalid input: " + str(e)
-    
+        
     except ValueError as e:
         return "Invalid input: " + str(e)
-safe_divide()    
+  
 
     
