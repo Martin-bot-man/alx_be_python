7c4cd6e19315782eff3b117551421f9ec8e91717
Martin Odongo <martinwere14@gmail.com>
2025-06-21 03:12:42 +0300

    feat: simple python calculator with its test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,14 +10,14 @@ class SimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2,3) ,5)
         self.assertEqual(self.calc.add(-1,1), 0) 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,3) ,-1)
         self.assertEqual(self.calc.subtract(5,4) ,1)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,4),8)
         self.assertEqual(self.calc.multiply(0,1),0)  
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4,2),2)
         self.assertEqual(self.calc.divide(12,-2),10)             
