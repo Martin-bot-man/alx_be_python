19af0b8e59ed9798a3b7b075b9cbfa254b015fdc
Martin Odongo <martinwere14@gmail.com>
2025-06-22 09:55:27 +0300

    feat: libray management system first commit

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,20 @@
+class Library:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out= False
+
+    def checked_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out=True
+            print(f"{self.title} has been checked out" )
+        else:
+            print(f"{self.title} is already checked out")    
+    def return_book(self):
+        if  self._is_checked_out:
+            self._is_checked_out=False
+            print(f"{self.title} is available")  
+        else :
+            print(f"{self.title} has been issued")
+    def _is_checked_out(self)
+        return self._is_checked_out              
